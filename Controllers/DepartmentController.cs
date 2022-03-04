bbfcd22b18927abb470b753a50eade97d2eaec6e
Rudy <rudy.mejia@sacsa-ca.com>
2022-03-04 15:50:18 -0600

    Full CRUD

--- Controllers/DepartmentController.cs
+++ Controllers/DepartmentController.cs
@@ -24,17 +24,88 @@ namespace TestAPI.Controllers
             this._context = context;
         }
 
+        /// <summary>
+        /// Obtiene el listado de departamentos registrados
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         public IEnumerable<Department> Get()
         {
             return _context.Department.ToList();
         }
 
+        /// <summary>
+        /// Obtiene un departamento especifico
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         [HttpGet("{id}")]
         public Department Get(int id)
         {
             var department = _context.Department.Find(id);
             return department;
+        }
+
+        /// <summary>
+        /// Insertamos un departamento
+        /// </summary>
+        /// <param name="department"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Post(Department department)
+        {
+            try
+            {
+                _context.Department.Add(department);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Modificamos un departamento
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="department"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, Department department)
+        {
+            if (department.DepartmentId == id)
+            {
+                _context.Department.Update(department);
+                _context.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        /// <summary>
+        /// Eliminamos un departamento
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public ActionResult Put(int id)
+        {
+            var department = _context.Department.Find(id);
+            if (department != null)
+            {
+                _context.Department.Remove(department);
+                _context.SaveChanges();
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
         }
     }
 }
