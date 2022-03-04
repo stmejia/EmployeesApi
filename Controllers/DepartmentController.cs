d9192bed3398f539599c6ce2c6c1d5e5be3a6e4f
Rudy <rudy.mejia@sacsa-ca.com>
2022-03-04 15:24:24 -0600

    First Commit

---
+++ Controllers/DepartmentController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAPI.Data;
+using TestAPI.Models;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ApplicationDBContext _context;
+
+        public DepartmentController(IConfiguration configuration, ApplicationDBContext context)
+        {
+            _configuration = configuration;
+            this._context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Department> Get()
+        {
+            return _context.Department.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Department Get(int id)
+        {
+            var department = _context.Department.Find(id);
+            return department;
+        }
+    }
+}
---
+++ Data/ApplicationDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAPI.Models;
+
+namespace TestAPI.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options):base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
